a2029b7fb590e02a04505b115113b9ab846948d0
Nand Patel <80486647+N-D-P@users.noreply.github.com>
2023-01-21 18:26:35 -0600

    Time: 21 ms (87.05%), Space: 13.2 MB (93.82%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> findOrder(int V, vector<vector<int>>& prerequisites) {
+        int cnt = 0;
+        vector<int> ind(V);
+        vector<int> adj[V];
+        queue<int> q;
+        vector<int> ans;
+        
+        for(int i=0; i<prerequisites.size(); i++)
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        
+        for(int i=0; i<V; i++)
+            for(int x : adj[i])
+                ind[x]++;
+        
+        for(int i=0; i<V; i++)
+            if(ind[i] == 0)
+                q.push(i);
+        
+        while(!q.empty()) {
+            int ele = q.front();
+            q.pop();
+            cnt++;
+            ans.push_back(ele);
+            for(int i:adj[ele]) {
+                ind[i]--;
+                if(ind[i] == 0)
+                    q.push(i);
+            }
+        }
+        vector<int> a;
+        return cnt == V ? ans : a;
+    }
+};
