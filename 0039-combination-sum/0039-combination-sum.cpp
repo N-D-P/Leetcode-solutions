07fcec6741033bb5d69824ff5aec93d10221b5d8
Nand Patel <80486647+N-D-P@users.noreply.github.com>
2023-01-26 16:46:16 -0600

    Time: 4 ms (84.29%), Space: 10.8 MB (81.78%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -3,18 +3,17 @@ public:
     void dfs(vector<int> &nums, int target, vector<vector<int>> &ans, vector<int> &temp, int ind) {
         if(target == 0) {
             ans.push_back(temp);
+            return;
         }
         else if(target < 0)
             return;
-        else {
-            for(int i=ind; i<nums.size(); i++) {
-                temp.push_back(nums[i]);
-                // target -= nums[i];
-                dfs(nums,target-nums[i],ans,temp,i);
-                // target += nums[i];
-                temp.pop_back();
-            }
-        }
+        if(ind == nums.size())
+            return;
+        temp.push_back(nums[ind]);
+        dfs(nums,target-nums[ind],ans,temp,ind);
+        temp.pop_back();
+        dfs(nums,target,ans,temp,ind+1);
+
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
