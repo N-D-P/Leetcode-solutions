e2f32a269560aa2ba624b1b6b51944544288e68c
Nand Patel <80486647+N-D-P@users.noreply.github.com>
2023-03-14 00:36:49 -0500

    Time: 4 ms (28.05%), Space: 9.2 MB (87.90%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -12,27 +12,23 @@
 class Solution {
 public:
     int sumNumbers(TreeNode* root) {
-        queue<pair<TreeNode*,int>> q;
-        q.push({root,root->val});
         int ans = 0;
-        while(!q.empty()) {
-            auto ele = q.front();
-            TreeNode *curr = ele.first;
-            int val1 = ele.second;
-            q.pop();
-            
-            if(curr->left) {
-                q.push({curr->left, (val1*10)+curr->left->val});
-            }
-            if(curr->right) {
-                q.push({curr->right, (val1*10)+curr->right->val});
-            }
-            
-            if(curr->left == nullptr and curr->right == nullptr) {
-                cout<<val1<<" ";
-                ans += val1;
-            }
-        }
+        sumnum(root,ans,0);
         return ans;
+    }
+    
+    void sumnum(TreeNode* root, int &ans, int num) {
+        if(root == nullptr)
+            return;
+        if(root->left == nullptr and root->right == nullptr) {
+            ans += (num*10)+root->val;
+            cout<<num<<" ";
+            return;
+        }
+        // else if(root->left == nullptr or root->right == nullptr)
+        //     return;
+        sumnum(root->left,ans,(num*10)+root->val);
+        sumnum(root->right,ans,(num*10)+root->val);
+        return;
     }
 };
